8377098446e5345fc7da0df0b44856e26acfebec
sujal22l <sujalpednekar2548@gmail.com>
2026-05-08 00:40:48 +0530

    node docker ansible app

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("👋 Welcome to my NodeJS Web Application!");
+    res.send("Welcome to my NodeJS Web Application!");
 });
 
 const PORT = 3000;
